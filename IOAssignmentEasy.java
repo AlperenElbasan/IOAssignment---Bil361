042259491af30c89fc5aef66e5ffd624f937d0f4
Alperen <aalperenelbasan@gmail.com>
2018-03-28 17:28:32 +0300

    Added IOAssignment for Bil361 Hw5

---
+++ IOAssignmentEasy.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class IOAssignmentEasy {
+	
+	static ArrayList<Integer> listA = new ArrayList<Integer>();
+	static ArrayList<Integer> listB = new ArrayList<Integer>();
+	static ArrayList<Integer> listC = new ArrayList<Integer>();
+	
+	static int[][] arr = new int[3][3];
+	
+	final static int FINISH = 4200;
+	
+	public static void main(String ...strings){
+		addJob('A', 30, 50, 1);
+		addJob('B', 25, 75, 2);
+		addJob('C', 50, 100, 3);
+		
+		
+		int waitTime = 0;
+		int aCount = 1, bCount = 1, cCount = 1;
+		for(int time=1; time < FINISH; time++){
+			if(waitTime == 0){ //Only it is available
+				if(!listA.isEmpty()){
+					listA.remove(0); waitTime = 30; System.out.println("A"+(aCount++) + " : "+ time);
+				}else if(!listB.isEmpty()){
+					listB.remove(0); waitTime = 25; System.out.println("B"+(bCount++) + " : "+ time);
+				}else if(!listC.isEmpty()){
+					listC.remove(0); waitTime = 50; System.out.println("C"+(cCount++) + " : "+ time);
+				}
+			}
+			for(int j=0; j < arr.length; j++){
+				if(time % arr[j][2] == 0){
+					switch(arr[j][0]){
+					case 'A':
+						listA.add(arr[j][2]); break;
+					case 'B':
+						listB.add(arr[j][2]); break;
+					case 'C':
+						listC.add(arr[j][2]); break;
+					default:
+						System.out.println("SMTH WRONG"); System.exit(-1);
+					}
+				} 
+			}
+			if(waitTime == 0){ //Only it is available
+				if(!listA.isEmpty()){
+					listA.remove(0); waitTime = 30; System.out.println("A"+(aCount++) + " : "+ time);
+				}else if(!listB.isEmpty()){
+					listB.remove(0); waitTime = 25; System.out.println("B"+(bCount++) + " : "+ time);
+				}else if(!listC.isEmpty()){
+					listC.remove(0); waitTime = 50; System.out.println("C"+(cCount++) + " : "+ time);
+				}
+			}
+			if(waitTime > 0) waitTime--;
+			
+		}
+	}
+	
+
+	public static void addJob(char c, int jobtime , int requesttime, int w){
+		int where = w-1;
+		arr[where][0] = c;	
+		arr[where][1] = jobtime;
+		arr[where][2] = requesttime;
+	}
+}
